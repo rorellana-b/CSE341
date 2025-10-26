c3f9b1bc094e5af2ac19ffb55e9f2b892b2bed3f
rorellana-b <ronalbalmoreorellanabonilla@192.168.1.5>
2025-10-25 22:26:17 -0600

    Adding Assigment 1

---
+++ server.js
@@ -0,0 +1,12 @@
+// express web server setup
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello ROnal!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
